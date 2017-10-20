db6fc5a6e6c0ee378e5e14abc4155d5b89aaa4cf
yanji1221 <yanji1221@gmail.com>
2017-10-20 22:23:54 +0800

    add test for status bar footer

--- src/test/java/seedu/address/ui/StatusBarFooterTest.java
+++ src/test/java/seedu/address/ui/StatusBarFooterTest.java
@@ -18,14 +18,16 @@ import org.junit.Test;
 import guitests.guihandles.StatusBarFooterHandle;
 import seedu.address.commons.events.model.AddressBookChangedEvent;
 import seedu.address.model.AddressBook;
+import seedu.address.model.ReadOnlyAddressBook;
 
 public class StatusBarFooterTest extends GuiUnitTest {
 
     private static final String STUB_SAVE_LOCATION = "Stub";
     private static final String RELATIVE_PATH = "./";
-    private static final int numberOfPerson = 5;
 
     private static final AddressBookChangedEvent EVENT_STUB = new AddressBookChangedEvent(new AddressBook());
+
+    private static final int numberOfPerson = EVENT_STUB.data.getPersonList().size();
 
     private static final Clock originalClock = StatusBarFooter.getClock();
     private static final Clock injectedClock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
