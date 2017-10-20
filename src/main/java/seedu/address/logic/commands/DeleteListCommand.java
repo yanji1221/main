92c65f9ed5a8891e9c4b8c49cd8506810120d16b
quangtdn <nmquang229@yahoo.com.vn>
2017-10-21 03:34:12 +0800

    add feature deleteList

---
+++ src/main/java/seedu/address/logic/commands/DeleteListCommand.java
@@ -0,0 +1,7 @@
+package seedu.address.logic.commands;
+
+/**
+ * Created by nguyenminhquang on 10/21/17.
+ */
+public class DeleteListCommand {
+}
---
+++ src/main/java/seedu/address/logic/parser/DeleteListCommandParser.java
@@ -0,0 +1,7 @@
+package seedu.address.logic.parser;
+
+/**
+ * Created by nguyenminhquang on 10/21/17.
+ */
+public class DeleteListCommandParser {
+}
