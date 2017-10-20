451be572d2578d34df41b667fd44045817b6453f
quangtdn <nmquang229@yahoo.com.vn>
2017-10-21 03:35:10 +0800

    add feature deleteList

--- src/main/java/seedu/address/logic/commands/DeleteListCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteListCommand.java
@@ -1,7 +1,66 @@
 package seedu.address.logic.commands;
 
+import java.util.*;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
+
 /**
- * Created by nguyenminhquang on 10/21/17.
+ * Deletes a person identified using it's last displayed index from the address book.
  */
-public class DeleteListCommand {
+public class DeleteListCommand extends UndoableCommand {
+
+    public static final String COMMAND_WORD = "deleteList";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the list of persons identified by the index numbers used in the last person listing.\n"
+            + "Parameters: INDICES (must be a positive integers)\n"
+            + "Example: " + COMMAND_WORD + " 1 2 4";
+
+    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Persons: %1$s";
+
+    private final List<Index> listTargetIndices;
+
+    public DeleteListCommand(List<Index> listTargetIndices) {
+        this.listTargetIndices = listTargetIndices;
+    }
+
+
+    @Override
+    public CommandResult executeUndoableCommand() throws CommandException {
+
+        List<ReadOnlyPerson> lastShownList = model.getFilteredPersonList();
+        for(Index targetIndex: listTargetIndices) {
+            if (targetIndex.getZeroBased() >= lastShownList.size()) {
+                throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            }
+        }
+
+        List<ReadOnlyPerson> listPersonsToDelete= new ArrayList<ReadOnlyPerson>();
+        for(Index targetIndex: listTargetIndices) {
+            ReadOnlyPerson personToDelete = lastShownList.get(targetIndex.getZeroBased());
+            listPersonsToDelete.add(personToDelete);
+        }
+
+        try {
+            for(ReadOnlyPerson personToDelete: listPersonsToDelete) {
+                model.deletePerson(personToDelete);
+            }
+        } catch (PersonNotFoundException pnfe) {
+            assert false : "The target person cannot be missing";
+        }
+
+        return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, listPersonsToDelete));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof DeleteCommand // instanceof handles nulls
+                && (this.listTargetIndices.containsAll(((DeleteListCommand) other).listTargetIndices)
+                    && ((DeleteListCommand) other).listTargetIndices.containsAll(this.listTargetIndices) )); // state check
+    }
 }
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -10,6 +10,7 @@ import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.DeleteListCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
@@ -61,6 +62,9 @@ public class AddressBookParser {
 
         case DeleteCommand.COMMAND_WORD:
             return new DeleteCommandParser().parse(arguments);
+
+        case DeleteListCommand.COMMAND_WORD:
+                return new DeleteListCommandParser().parse(arguments);
 
         case ClearCommand.COMMAND_WORD:
             return new ClearCommand();
