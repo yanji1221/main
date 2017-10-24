b263759b15b734f474d82ae3cfc8df091bf2d23a
quangtdn <nmquang229@yahoo.com.vn>
2017-10-24 17:28:44 +0800

    modify parser for ProfilePage

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,6 +11,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_BIRTHDAY = new Prefix("b/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
+    public static final Prefix PREFIX_PROFILEPAGE = new Prefix("pr/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_DATE = new Prefix("d/");
     public static final Prefix PREFIX_DESCRIPTION = new Prefix("de/");
