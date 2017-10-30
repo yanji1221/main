5efa512e815a12c354e97d48ff3f52829fb9509e
quangtdn <nmquang229@yahoo.com.vn>
2017-10-30 17:50:23 +0800

    update AddCommandParserTest

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -135,10 +135,6 @@ public class AddCommandParserTest {
         assertParseFailure(parser, AddCommand.COMMAND_WORD + NAME_DESC_BOB + PHONE_DESC_BOB
                 + EMAIL_DESC_BOB + BIRTHDAY_DESC_BOB + VALID_ADDRESS_BOB + PROFILE_DESC_BOB , expectedMessage);
 
-        // missing profile prefix
-        assertParseFailure(parser, AddCommand.COMMAND_WORD + NAME_DESC_BOB + PHONE_DESC_BOB
-                + EMAIL_DESC_BOB + BIRTHDAY_DESC_BOB + ADDRESS_DESC_BOB + VALID_PROFILE_BOB , expectedMessage);
-
         // all prefixes missing
         assertParseFailure(parser, AddCommand.COMMAND_WORD + VALID_NAME_BOB + VALID_PHONE_BOB
                 + VALID_EMAIL_BOB + VALID_BIRTHDAY_BOB + VALID_ADDRESS_BOB + VALID_PROFILE_BOB, expectedMessage);
