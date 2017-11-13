3ef739dbd8dbe282beb9cc662186d84ce0b994aa
yanji1221 <yanji1221@gmail.com>
2017-11-13 12:42:18 +0800

    update collate

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -148,11 +148,6 @@ public class AddCommandParserTest {
         assertParseFailure(parser, AddCommand.COMMAND_WORD + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
                         + BIRTHDAY_DESC_BOB + INVALID_ADDRESS_DESC + PROFILE_DESC_BOB + TAG_DESC_HUSBAND
                         + TAG_DESC_FRIEND, Address.MESSAGE_ADDRESS_CONSTRAINTS);
-        /*
-        // invalid profile page
-        assertParseFailure(parser, AddCommand.COMMAND_WORD + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                        + BIRTHDAY_DESC_BOB + ADDRESS_DESC_BOB + INVALID_PROFILE_DESC + TAG_DESC_HUSBAND
-                         + TAG_DESC_FRIEND, ProfilePage.MESSAGE_PROFILEPAGE_CONSTRAINTS); */
 
         // invalid tag
         assertParseFailure(parser, AddCommand.COMMAND_WORD + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
